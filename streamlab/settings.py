14c46d0a70d11a7aa794012fc7ff6f6eb7cdc0f9
oluwatosin-1 <ajayijohnoluwadamilare@gmail.com>
2025-03-30 03:45:38 +0100

    latest push to server

--- streamlab/settings.py
+++ streamlab/settings.py
@@ -164,8 +164,11 @@ SRS_API_PORT = 1985
 # settings.py
 # CELERY_BROKER_URL = 'redis://redis:6379/0'
 # CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = "redis://streamlab_redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://streamlab_redis:6379/0"
+
+# CELERY_BROKER_URL = "redis://localhost:6379/0"
+# CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
